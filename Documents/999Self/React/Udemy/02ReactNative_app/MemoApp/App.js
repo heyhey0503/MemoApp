ebd84c1885362d1f8419143eb5662b9dc753bbc6
heyhey0503 <heyhey@takram.com>
2018-04-03 08:31:36 +0900

    Create Memoscreen

--- Documents/999Self/React/Udemy/02ReactNative_app/MemoApp/App.js
+++ Documents/999Self/React/Udemy/02ReactNative_app/MemoApp/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
+
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hi My name is Shohei</Text>
+        <Appbar />
+        <MemoListScreen />
       </View>
     );
   }
@@ -14,8 +19,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
+    backgroundColor: '#eeeeee'
   },
 });
